3f66328a5d0cb49150947efee37ffe05fba19498
ek <1936816942@qq.com>
2026-03-13 21:04:24 +0800

    PA1 watch step1: extend WP struct and APIs

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -8,8 +8,19 @@ typedef struct watchpoint {
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
+  char expr[128];
+  uint32_t last_val;
 
 
 } WP;
+
+void init_wp_pool(void);
+WP* new_wp(void);
+void free_wp(WP *wp);
+WP* watchpoint_head(void);
+WP* add_watchpoint(const char *expr, bool *success);
+bool delete_watchpoint(int no);
+void display_watchpoints(void);
+bool check_watchpoints(void);
 
 #endif
