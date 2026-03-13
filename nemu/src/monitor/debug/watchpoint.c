f635bf97dfaddf840e3f690d3720b5b3c7b67e81
2311605-Wang Wei <1936816942@qq.com>
2026-03-13 21:29:42 +0800

    PA1 watch step2: implement wp pool alloc/free

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -20,4 +20,44 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP* watchpoint_head(void) {
+  return head;
+}
+
+WP* new_wp(void) {
+  Assert(free_ != NULL, "No free watchpoint available");
+
+  WP *wp = free_;
+  free_ = free_->next;
+
+  wp->next = head;
+  head = wp;
+
+  wp->expr[0] = '\0';
+  wp->last_val = 0;
+
+  return wp;
+}
+
+void free_wp(WP *wp) {
+  WP *prev = NULL;
+  WP *cur = head;
+  while (cur != NULL && cur != wp) {
+    prev = cur;
+    cur = cur->next;
+  }
+
+  Assert(cur == wp, "watchpoint %d is not in active list", wp->NO);
+
+  if (prev == NULL) {
+    head = cur->next;
+  }
+  else {
+    prev->next = cur->next;
+  }
+
+  cur->next = free_;
+  free_ = cur;
+}
+
 
