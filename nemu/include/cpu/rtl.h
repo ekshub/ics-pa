fbe369622d4a032c7bdfb9d9dfb93fa827c5e873
2311605-Wang Wei <1936816942@qq.com>
2026-04-09 17:28:11 +0800

    pa2-6: implement all remaining RTL pseudo instructions

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -127,59 +127,73 @@ make_rtl_setget_eflags(IF)
 
 static inline void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1) {
   // dest <- src1
-  TODO();
+  *dest = *src1;
 }
 
 static inline void rtl_not(rtlreg_t* dest) {
   // dest <- ~dest
-  TODO();
+  *dest = ~(*dest);
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  if (width == 4) {
+    *dest = *src1;
+  } else {
+    int shift = 32 - width * 8;
+    *dest = (int32_t)(*src1 << shift) >> shift;
+  }
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
-  TODO();
+  cpu.esp -= 4;
+  vaddr_write(cpu.esp, 4, *src1);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
-  TODO();
+  *dest = vaddr_read(cpu.esp, 4);
+  cpu.esp += 4;
 }
 
 static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 == 0 ? 1 : 0)
-  TODO();
+  *dest = (*src1 == 0) ? 1 : 0;
 }
 
 static inline void rtl_eqi(rtlreg_t* dest, const rtlreg_t* src1, int imm) {
   // dest <- (src1 == imm ? 1 : 0)
-  TODO();
+  *dest = (*src1 == imm) ? 1 : 0;
 }
 
 static inline void rtl_neq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 != 0 ? 1 : 0)
-  TODO();
+  *dest = (*src1 != 0) ? 1 : 0;
 }
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
-  TODO();
+  uint32_t mask = 1U << (width * 8 - 1);
+  *dest = (*src1 & mask) ? 1 : 0;
 }
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
-  TODO();
+  if (width == 4) {
+    cpu.ZF = (*result == 0);
+  } else {
+    uint32_t mask = (1U << (width * 8)) - 1;
+    cpu.ZF = ((*result & mask) == 0);
+  }
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
-  TODO();
+  uint32_t mask = 1U << (width * 8 - 1);
+  cpu.SF = (*result & mask) ? 1 : 0;
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
