fc3934b6f9eb740afaac6a13062755eb1c147a04
tracer-ics2017 <tracer@njuics.org>
2026-04-19 02:41:05 -0700

    >  compile 2311605 system Linux system-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  02:41:05 up  2:56,  1 user,  load average: 0.06, 0.09, 0.04 d62ffdd4af6a2fce277a89cb466b50a6f15e70a4

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -7,7 +7,14 @@ make_EHelper(test) {
 }
 
 make_EHelper(and) {
-  TODO();
+  rtl_and(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+
+  rtl_update_ZFSF(&t2, id_dest->width);
+  rtl_update_PF(&t2);
+
+  rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
 
   print_asm_template2(and);
 }
