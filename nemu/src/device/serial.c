33e382dada0fecc9e712f96574858c8e1de1d8c5
tracer-ics2017 <tracer@njuics.org>
2026-04-27 18:23:10 -0700

    >  compile 2311605 system Linux system-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  18:23:10 up  3:22,  1 user,  load average: 0.24, 0.09, 0.21 3fe42972e74fca879e29a441b1a220c26f609a5d

--- nemu/src/device/serial.c
+++ nemu/src/device/serial.c
@@ -11,12 +11,15 @@ static uint8_t *serial_port_base;
 
 void serial_io_handler(ioaddr_t addr, int len, bool is_write) {
   if (is_write) {
+    assert(len == 1);
     if (addr == SERIAL_PORT + CH_OFFSET) {
-      char c = serial_port_base[CH_OFFSET];
-      /* We bind the serial port with the host stdout in NEMU. */
-      putc(c, stdout);
-      if (c == '\n') {
-        fflush(stdout);
+      if ((serial_port_base[3] & 0x80) == 0) {
+        char c = serial_port_base[CH_OFFSET];
+        /* We bind the serial port with the host stdout in NEMU. */
+        putc(c, stdout);
+        if (c == '\n') {
+          fflush(stdout);
+        }
       }
     }
   }
